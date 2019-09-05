1535e647573a8868a5487996a2df1c8ddc79b8e2
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-09-04 17:03:06 -0700

    changed Room.initialize to accept all rooms with positive room numbers, instead of limiting to the number of rooms that happen to exist in our example hotel

--- lib/room.rb
+++ lib/room.rb
@@ -4,8 +4,8 @@ module HotelBooking
     attr_reader :number, :reservations
     
     def initialize(room_number)
-      unless room_number.to_i > 0 && room_number.to_i < 21
-        raise ArgumentError.new("Invalid room number; expected an integer between 1 and 20, received #{room_number}")
+      unless room_number.to_i > 0
+        raise ArgumentError.new("Invalid room number; expected a positive Integer, received #{room_number}")
       end
       
       @number = room_number
